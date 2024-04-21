8495c9caffdf46dae038f352313be22612c41c0e
Shunsuke Shimizu <grafi@grafi.jp>
2024-04-21 14:28:42 +0900

    add missed sayhisort_cpp20_test

---
+++ tests/sayhisort_cpp20_test.cc
@@ -0,0 +1,14 @@
+#include "sayhisort.h"
+
+#include <array>
+#include <functional>
+
+int main(int argc, char** argv) {
+    constexpr std::array<int, 9> a = ([]() {
+        std::array pi{3, 1, 4, 1, 5, 9, 2, 6, 5};
+        sayhisort::sort(pi.begin(), pi.end(), std::less<int>{});
+        return pi;
+    })();
+    static_assert(a == std::array{1, 1, 2, 3, 4, 5, 5, 6, 9});
+    return 0;
+}
